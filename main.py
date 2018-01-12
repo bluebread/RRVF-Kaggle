7e714c6ecf0d34c3eafb1543c527da81854dbea4
bluebread <hotbread70127@gmail.com>
2018-01-12 11:34:29 +0800

    reserve process completed

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ THE MAIN ALGORITHM FRAME
 from process import store_process
 
 if __name__ == '__main__':
-    store_process()
+    store_df = store_process()
--- process.py
+++ process.py
@@ -1,14 +1,21 @@
 """
 数据集预处理
-- data_process:
+- store_process:
     将各家商店的信息进行整合。
-    输出store_df，包含 air_store_id、hpg_store_id、latitude、longitude、genre、area_name。
+    返回store_df，包含 air_store_id, hpg_store_id,
+    latitude, longitude, genre, area_name。
     store_df 将另外输出成 store_info.csv。
     若只被单边数据集登录，则该商店 id 信息将补 0 。
+- reserve_process:
+    预定信息处理，并将数据集合并。
+    返回reserve_df，包含 id(air & hpg), visit_datetime,
+    total_reserve。
 - clustering:
     依据类型分组统计。
-    输出genre_df、city_df、lati_long_df。
+    返回genre_df、city_df、lati_long_df。
 """
+from datetime import timedelta, datetime
+
 import pandas as pd
 import numpy as np
 
@@ -127,7 +134,79 @@ def _area_name_process(store_df):
     return store_df
 
 
-def clustering():
+def reserve_process():
     """
     """
-    pass
+    ar_df = pd.read_csv(
+        'csv/air_reserve.csv',
+        parse_dates=['visit_datetime', 'reserve_datetime'])
+    hr_df = pd.read_csv(
+        'csv/hpg_reserve.csv',
+        parse_dates=['visit_datetime', 'reserve_datetime'])
+    r_pro_dfs = []
+    for name, df in [('air', ar_df), ('hpg', hr_df)]:
+        reserve_df = _r_df_process(name, df)
+        csv_name = '{0}_proc_res.csv'.format(name)
+        reserve_df.to_csv(csv_name, encoding='utf-8', index=False)
+        r_pro_dfs.append(reserve_df)
+
+    return r_pro_dfs
+
+
+def _r_df_process(name, df):
+    store_id_name = '{0}_store_id'.format(name)
+    df = (df.drop(['reserve_datetime'], axis=1)
+          .sort_values([store_id_name, 'visit_datetime']))
+    store_list = df[store_id_name].unique()
+
+    reserve_df = pd.DataFrame([], columns=['id', 'visit_date', 'visitors'])
+    for store_id in store_list:
+        print('{0} has been procceded!'.format(store_id))
+        sid_df = df[df[store_id_name] == store_id]
+        reserve_indi_df = _store_process(store_id, sid_df)
+        reserve_df = pd.concat([reserve_df, reserve_indi_df])
+    reserve_df.rename(columns={'id': store_id_name})
+
+    return reserve_df
+
+
+def _store_process(store_id, sid_df):
+    date_list = sid_df['visit_datetime'].unique()
+    unique_date_list = _unique_date(date_list)
+    sid_df.set_index('visit_datetime', inplace=True)
+    
+    reserve_arr = []
+    for date in unique_date_list:
+        nxt = date + timedelta(days=1)
+        visit_num = sid_df['reserve_visitors'][date:nxt].sum()
+        if visit_num is not 0:
+            reserve_arr.append([date, visit_num])
+
+    reserve_indi_df = pd.DataFrame(
+        reserve_arr,
+        columns=['visit_date', 'visitors'])
+    reserve_indi_df['id'] = store_id
+
+    return reserve_indi_df
+
+
+def _npdatetime64_convert_to_datetime_date(npdatetime64):
+    """numpy.datetime64 转换成 datetime，只传回 date 部份"""
+    unix_epoch = np.datetime64(0, 's')
+    one_second = np.timedelta64(1, 's')
+    seconds_since_epoch = (npdatetime64 - unix_epoch) / one_second
+    return datetime.utcfromtimestamp(seconds_since_epoch).date()
+
+
+def _unique_date(date_list):
+    unique_date_arr = []
+    for d in date_list:
+        convert_d = _npdatetime64_convert_to_datetime_date(d)
+        unique_date_arr.append(convert_d)
+    return pd.Series(unique_date_arr).unique()
+
+
+if __name__ == '__main__':
+    ar_pro_df, hr_pro_df = reserve_process()
+    print(ar_pro_df.head())
+    print(hr_pro_df.head())
